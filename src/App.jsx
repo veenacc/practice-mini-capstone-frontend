ae04e94644f4aae5c7420bac351f145d6a2bf639
veenacc <veena.chenicheri@gmail.com>
2024-05-30 12:48:06 -0400

    added html for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Header() {
+  return(
+    <div>
+      <header> 
+        <a href="#">Home</a> | <a href="#products-index">All Products</a> | <a href="#products-new">New Product</a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+function ProductsNew() {
+  return(
+    <div>
+      
+      <h1>New Product</h1>
+
+      <form>
+        <label htmlFor="title">Name</label><br />
+        <input type="text" id="title" name="title"></input><br />
+        {/* <label htmlFor="body">Body</label><br />
+        <input type="text" id="body" name="body"></input><br /> */}
+        <label htmlFor="image">Image</label><br />
+        <input type="url"></input>
+        {/*Can add id and name */}
+      </form>
+    </div>
+  );
+}
+
+function ProductsIndex() {
+
+  // let products = [{id:1, name:"apple", image:"https://t3.ftcdn.net/jpg/01/76/97/96/360_F_176979696_hqfioFYq7pX13dmiu9ENrpsHZy1yM3Dt.jpg"}];
+
+  return (
+    <div>
+      <h1>Apples</h1>
+      <img src="https://t3.ftcdn.net/jpg/01/76/97/96/360_F_176979696_hqfioFYq7pX13dmiu9ENrpsHZy1yM3Dt.jpg"></img>
+      <br />
+      <h1>Oranges</h1>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/Ambersweet_oranges.jpg/800px-Ambersweet_oranges.jpg" height="400" width="400" ></img>
+      <br />
+    </div>
+  );
+
+}
+
+function Footer() {
+  return(
+    <div>
+       <footer>
+      <p>Copyright 20XX</p>
+      </footer>
+    </div>
+  );
+}
+
+function Content() {
+  return (
+    <main>
+      <ProductsNew />
+      <ProductsIndex />
+    </main>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
